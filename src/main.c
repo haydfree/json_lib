fa6be931b926447a28cc76388dcb42ebfc287715
Hayden Freeman <haydenfreeman@yahoo.com>
2024-12-30 08:45:18 -0600

    omg omg omg

--- src/main.c
+++ src/main.c
@@ -4,56 +4,58 @@
 #include "json_lib.h"
 
 char *escapeJsonStr(const char *js) {
-    int i; 
+    int i, j = 0; 
     int len = strlen(js);
     char *output = malloc(len * 2 + 1);
     for (i = 0; i < len; i++) {
         switch (js[i]) {
             case '\\': 
-                output[i] = '\\';
-                output[i + 1] = '\\';
+                output[j++] = '\\';
+                output[j++] = '\\';
                 break;
             case '\"':
-                output[i] = '\\';
-                output[i + 1] = '\"';
+                output[j++] = '\\';
+                output[j++] = '\"';
                 break;
             case '\n':
-                output[i] = '\\';
-                output[i + 1] = '\n';
+                output[j++] = '\\';
+                output[j++] = '\n';
                 break;
             case '\b':
-                output[i] = '\\';
-                output[i + 1] = '\b';
+                output[j++] = '\\';
+                output[j++] = '\b';
                 break;
             case '\f':
-                output[i] = '\\';
-                output[i + 1] = '\f';
+                output[j++] = '\\';
+                output[j++] = '\f';
                 break;
             case '\r':
-                output[i] = '\\';
-                output[i + 1] = '\r';
+                output[j++] = '\\';
+                output[j++] = '\r';
                 break;
             case '\t':
-                output[i] = '\\';
-                output[i + 1] = '\t';
+                output[j++] = '\\';
+                output[j++] = '\t';
                 break;
             default:
+                output[j++] = js[i];
                 break;
         }
     }
 
-    output[len] = '\0';
+    output[j] = '\0';
     return output;
 }
 
 int main() {
-    
 
     const char *str = "hello \n buddy \t guy";
-    printf("str: %s", str);
-
     str = escapeJsonStr(str);
-    printf("str: %s", str);
+
+    int i;
+    for (i = 0; i < (int) strlen(str); i++) {
+        printf("char: %c\n", str[i]);
+    }
 
     return 0;
 }
